c3e0ad5dbb76caf833ad1c8b700ad93f4abb1289
extroverter-542 <sanjisama62728@genvolvo.lol>
2024-10-27 09:05:03 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,10 +1,6 @@
 #(©)CodeXBotz
-
-
-
-
 import pymongo, os
-from config import DB_URI, DB_NAME
+from config import DB_URI, DB_NAME, ADMINS
 
 
 dbclient = pymongo.MongoClient(DB_URI)
@@ -13,11 +9,15 @@ database = dbclient[DB_NAME]
 
 user_data = database['users']
 
-
+req_one = database['req_one']  
+req_two = database['req_two']
 
 async def present_user(user_id : int):
     found = user_data.find_one({'_id': user_id})
-    return bool(found)
+    if found:
+        return True
+    else:
+        return False
 
 async def add_user(user_id: int):
     user_data.insert_one({'_id': user_id})
@@ -34,3 +34,49 @@ async def full_userbase():
 async def del_user(user_id: int):
     user_data.delete_one({'_id': user_id})
     return
+
+
+async def is_requested_one(message):
+    user = await get_req_one(message.from_user.id)
+    if user:
+        return True
+    if message.from_user.id in ADMINS:
+        return True
+    return False
+    
+async def is_requested_two(message):
+    user = await get_req_two(message.from_user.id)
+    if user:
+        return True
+    if message.from_user.id in ADMINS:
+        return True
+    return False
+
+async def add_req_one(user_id):
+    try:
+        if not await get_req_one(user_id):
+            await req_one.insert_one({"user_id": int(user_id)})
+            return
+    except:
+        pass
+        
+async def add_req_two(user_id):
+    try:
+        if not await get_req_two(user_id):
+            await req_two.insert_one({"user_id": int(user_id)})
+            return
+    except:
+        pass
+
+async def get_req_one(user_id):
+    return req_one.find_one({"user_id": int(user_id)})
+
+async def get_req_two(user_id):
+    return req_two.find_one({"user_id": int(user_id)})
+
+async def delete_all_one():
+    req_one.delete_many({})
+
+async def delete_all_two():
+    req_two.delete_many({})
+
